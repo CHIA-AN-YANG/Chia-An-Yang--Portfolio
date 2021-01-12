9d617a041aef00ba2803e760286825ebbb9ce7ce
Chia-An Yang <71560280+CHIA-AN-YANG@users.noreply.github.com>
2021-01-12 22:06:12 +0800

    Add files via upload

--- js/customize.js
+++ js/customize.js
@@ -1,16 +1,30 @@
 
 //nav bar appeare
-$(window).mousemove(function(evt){
-    if(evt.pageY>=$(window).height()){
-        $('nav').addClass('show');
-        $('nav').css('top','0px');        
-    }
-    if(evt.pageY<$(window).height()){
-        $('nav').removeClass('show');  
-    }
+$(window).scroll(function(){
+    const pageY = $(window).scrollTop()
+    if(pageY>=100){
+        $('.nav-btm').hide();
+        $('.nav-top').fadeIn(500);
+        $('.nav-top').css('top','0px'); 
+        $('nav').css('background-color','#FFF')
+    }else{
+        $('.nav-top').hide();
+        $('.nav-btm').fadeIn(500);
+        $('nav').css('background-color','transparent')}
 })
 
+//hover
+$('ul.nav-btm a').each(function(idx,el){
+    $(el).on('mouseenter',()=>{
+        $(el).parent().css('height','45px')
+    });
+    $(el).on('mouseleave',()=>{
+        $(el).parent().css('height','60px')
+    })
 
+}
+
+)
 // ham menu
 $('.frame_ham').click(function(evt){
 
@@ -22,3 +36,19 @@ $('.frame_ham').click(function(evt){
         evt.stopPropagation();
         $('.frame_ham > ul').removeClass('open');
 })
+
+//show effect
+const in1 = $('.intro-1>.container>.row')
+const in2 = $('.intro-2')
+showUp(in1);
+showUp(in2);
+function showUp(el){
+    el.hide();
+    $(window).scroll(function(){
+        const secY = el.offset().top;
+        const pageY = $(window).scrollTop();
+        if(pageY>secY){
+            el.slideDown(2000).delay(1500).fadeIn(500);
+        }
+    })
+}
